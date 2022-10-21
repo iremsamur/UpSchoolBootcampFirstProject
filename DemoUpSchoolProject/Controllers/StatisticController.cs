3d82eb30f6e4e8e5eb0c646650eb806de4b4da12
iremsmr <iremsamur129@gmail.com>
2022-10-21 19:03:54 +0300

    Statistic page added

---
+++ DemoUpSchoolProject/Controllers/StatisticController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoUpSchoolProject.Models.Entities;
+
+namespace DemoUpSchoolProject.Controllers
+{
+    public class StatisticController : Controller
+    {
+        UpSchoolDbPortfolioEntities db = new UpSchoolDbPortfolioEntities();
+        
+        // GET: Statistic
+        public ActionResult Index()
+        {
+            ViewBag.v1= db.TblTestimonial.Count();//referansların toplam sayısı
+            ViewBag.v2 = db.TblTestimonial.Where(x => x.City == "İstanbul").Count();//istanbuldaki referans sayısı
+            //yazılım mühendisi haricindeki kişi sayısı
+            ViewBag.v3 = db.TblTestimonial.Where(x => x.Profession != "Yazılım Mühendisi").Count();
+            //şehri trabzon olanın kişi ismini getiren sorgu
+            ViewBag.v4 = db.TblTestimonial.Where(x => x.City == "Trabzon").Select(y => y.NameSurname).FirstOrDefault();//sadece select içinde belirtilen
+            //sütunu getirir
+            //tek değerin dönmesi gereken durumda FirstorDefault kullanılır.
+            //tüm değerler için tolist olur.
+            //referansların ortalama maaşı
+            ViewBag.v5 = db.TblTestimonial.Average(x => x.Balance);
+
+            return View();
+        }
+    }
+}
---
+++ DemoUpSchoolProject/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoUpSchoolProject.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //kolay yönetebilmek için sayfayı parçalara ayıralım
+        public PartialViewResult Partial1()
+        {
+            //View'leri küçük parçalara ayırmak için kullanılan yapı
+            return PartialView();
+        }
+        public PartialViewResult HeadPartial()
+        {
+            //partial view içinde de tüm veritabanı işlemlerini gerçekleştirebiliriz
+            return PartialView();
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            
+            return PartialView();
+        }
+    }
+}
